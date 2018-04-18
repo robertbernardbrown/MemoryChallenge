74bb95d53b411cdcf957c28830b27a3e92aa633d
Bob Brown <robertbernardbrown@gmail.com>
2018-04-18 11:55:07 -0400

    adds footer, header, and wrapper components

---
+++ memorychallenge/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+    <footer className="footer">
+        <div className="container">
+            Panel body
+        </div>
+    </footer>
+)
+
+export default Footer;
---
+++ memorychallenge/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => (
+    <div className="jumbotron">
+        <h1>Memory Game</h1>
+        <p>Pictures will change position after each click. Click a new picture each round to gain a point!</p>
+    </div>
+)
+
+export default Header;
